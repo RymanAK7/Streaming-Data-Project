fdb17a35c71a6b88839ba674a3e0d9bc6e1f0c1f
RymanAk <Rymanak@icloud.com>
2024-06-17 03:08:07 +0100

    Include dynamic Kinesis stream name in endpoint_url output

--- infrastructure/output.tf
+++ infrastructure/output.tf
@@ -1,3 +1,3 @@
 output "endpoint_url" {
-  value = "${aws_api_gateway_stage.example.invoke_url}/${var.endpoint_path}?search_term=Python&kinesis_stream=test_stream&from_date=2024-01-01"
+  value = "${aws_api_gateway_stage.example.invoke_url}/${var.endpoint_path}?search_term=Python&kinesis_stream=${var.kinesis_stream_name}&from_date=2024-01-01"
 }
